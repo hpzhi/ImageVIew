4ab4539cc2cf7bae28ef5f5cd487637a4ae2be9a
hpzhi <huperzh@gmail.com>
2018-04-03 23:02:39 +0800

    modified pictureshowdialog.cpp and resource.qrc

--- pictureshowdialog.cpp
+++ pictureshowdialog.cpp
@@ -21,8 +21,8 @@
 #define FULL 3
 #define NORMAL 4
 #define DELE 5
-const char* PICPATH = "";
-const char* THUMBPATH = "";
+const char* PICPATH = ":/resource/images";
+const char* THUMBPATH = ":/resource/images/thumb";
 const int SCREENW = 1920;
 //#define PICPATH  "/mnt/udisk/DCIM/"
 #define ImageTitle "picture"
